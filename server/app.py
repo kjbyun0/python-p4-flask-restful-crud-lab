db89ab8f956866f04a3b91058ebe16afd001c5c3
KI JONG BYUN <kjbyun0@gmail.com>
2024-02-24 13:57:43 -0600

    Assignment Done

--- server/app.py
+++ server/app.py
@@ -46,7 +46,24 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        request_dict = request.get_json()
+        for attr in request_dict:
+            setattr(plant, attr, request_dict.get(attr))
+        db.session.add(plant)
+        db.session.commit()
 
+        return make_response(plant.to_dict(), 200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+
+        response_body = {}
+        return make_response(response_body, 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
